4e5e81d900bb2ad68d2b78c0054b63c32052b98a
Deelenta <ST10117042@vcconnect.edu.za>
2023-03-24 13:15:05 +0200

    Add project files.

---
+++ StudentApplication/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StudentApplication;
+
+Console.WriteLine("Please enter your student number: ");
+int studentNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(studentNumber);
+
+//String studentNumber = "111";
+
+
---
+++ StudentApplication/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication
+{
+    public class Student
+    {
+        //VARIABLES
+        private String StudentNumber;
+
+        //COBSTRUCTOR
+        public Student(string studentNumber, string name, string surname)
+        {
+            this.StudentNumber = studentNumber;
+        }
+
+        //GETTER AND SETTERS
+        public void setStudentNumber(String studentNumber)
+        {
+            StudentNumber = studentNumber;
+        }
+
+        public String getStudentNumber()
+        {
+            return StudentNumber;
+        }
+
+    }
+}
